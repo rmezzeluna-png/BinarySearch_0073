94e756faf2ae54807bbad35920dc689d512b27a6
rmezzeluna-png <rmezzeluna@gmail.com>
2026-04-16 17:15:23 +0700

    prosedur

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -4,3 +4,26 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input()
+{
+    while(true)
+    {
+        cout << "masukkan banyaknya elemen pada array(maksimal 10) : " ;
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else{
+            cout << "\n[!] jumlah elemen tidak boleh lebidh dari 10. silahkan coba lagi.\n";
+        }
+    }
+    
+    cout << "\n================================\n";
+    cout << "        masukan elemen array      \n";
+    cout << "\n================================\n";
+    cout << endl;
+    
+}
