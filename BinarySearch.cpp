e67f7789618c23d1720102c6398978e276bc82c0
rmezzeluna-png <rmezzeluna@gmail.com>
2026-04-16 17:28:01 +0700

    perulangan+penentuan

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -39,7 +39,15 @@ void bublesortarray()
     {
     {
         for(int j = 0; j <= nPanjang - 1 - pass; j++)
-        
-    }
+        {
+            if (element [j] > element [j + 1])
+            {
+                int temp = element [j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
     
 }
